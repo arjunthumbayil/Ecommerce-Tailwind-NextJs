e2fef01a8937dcb46fee482a670f0184474114e6
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-10-22 22:36:22 +0530

    Products from data.js listed on home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import Layout from "../components/Layout";
+import ProductItems from "../components/ProductItems";
+import data from "../utils/data";
 
 export default function Home() {
-  return <Layout title="Home Page">homepage</Layout>;
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItems product={product} key={product.slug}></ProductItems>
+        ))}
+      </div>
+    </Layout>
+  );
 }
